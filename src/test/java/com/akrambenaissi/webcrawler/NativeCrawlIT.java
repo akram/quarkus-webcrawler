3545e3572e1c9a8d1c2cbd71ab01f777271d36a3
Akram Ben Aissi <akram.benaissi@gmail.com>
2019-03-18 17:41:30 +0100

    initial commit

---
+++ src/test/java/com/akrambenaissi/webcrawler/NativeCrawlIT.java
@@ -0,0 +1,9 @@
+package com.akrambenaissi.webcrawler;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeCrawlIT extends CrawlTest {
+
+    // Execute the same tests but in native mode.
+}
